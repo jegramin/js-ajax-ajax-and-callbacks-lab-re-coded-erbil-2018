f4f867c5a0634d3286e3f50772041b8a66acd484
Mohammed Ameen <jegroamin@gmail.com>
2018-09-12 19:53:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let searchRepositories = ()=>{
     $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data =>{
       $('#results').innerHTML = `<div>
           <p>name: ${data.name}, description: ${data.description}, repository owner login: ${data.owner.login}</p>
-          <a href="${result.html_url}">${result.name}</a>
+          <a href="${data.html_url}">${data.name}</a>
+          
       </div>`;
       
     })
