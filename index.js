9e8992ccb87d35e6a178273a1fdc72dda7550163
Mohammed Ameen <jegroamin@gmail.com>
2018-09-12 19:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let searchRepositories = ()=>{
     let searchTerms = document.getElementById("searchTerms").value;
-    .get()
-    $(#results).innerHTML = 
+    .get(`https://api.github.com/search/repositories?q=${searchTerms}`, data =>{
+      $(#results).innerHTML = data;
+    })
+    
   }
 
 $(document).ready(function (){
