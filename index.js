636e5a4c55f320a5c99cc8dd676517a6e0cb13d3
Mohammed Ameen <jegroamin@gmail.com>
2018-09-12 19:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let searchRepositories = ()=>{
     let searchTerms = document.getElementById("searchTerms").value;
     $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data =>{
-      $('#results').innerHTML = `<p>name: ${data.name}, description: ${data.description}, repository owner login: ${data.owner.login}</p>`;
+      $('#results').innerHTML = `<div>
+          <p>name: ${data.name}, description: ${data.description}, repository owner login: ${data.owner.login}</p>
+          <a href="${result.html_url}">${result.name}</a>
+      </div>`;
       
     })
     
